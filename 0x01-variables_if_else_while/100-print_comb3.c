5140edeabd78fc888280dc585853d06d355da799
ezzeldien <ezzeldienfarrag@gmail.com>
2023-03-25 03:19:23 +0200

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
  * main - Entry point
  *
  * Description: print all possible different
- * 		combinations of two digits.
+ * combinations of two digits.
  *
  * Return: Always 0 (Success)
 */
@@ -12,15 +12,14 @@ int main(void)
 	int digit1 = 0, digit2;
 
 	while (digit1 <= 9)
-         {
-                digit2 2 = 0
-                while (digit2 <= 9)
+	{
+		digit2 2 = 0
+		while (digit2 <= 9)
 		{
 			if (digit1 != digit2 && digit1 < digit2)
 			{
 				putchar (digit1 + 48);
 				putchar (digit2 + 48);
-				
 				if (digit1 + digit2 != 17)
 				{
 					putchar (',');
@@ -32,6 +31,5 @@ int main(void)
 		digit1++;
 	}
 	putchar('\n');
-	
 	return (0);
 }
